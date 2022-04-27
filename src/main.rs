9e0b962c097f6e2a23a9b471fc89d965b2369e50
Carlos Morgado <carlos primatas.org>
2022-04-27 22:32:13 +0100

    exec/ret mockup

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ tungstenite::{Error, Result},
 use url::Url;
 
 use minion_msg;
+use minion_msg::{MinionOps, MinionMsg, MinionId};
 
 
 #[tokio::main]
@@ -18,8 +19,8 @@ async fn main() {
     println!("WebSocket handshake has been successfully completed");
     
     let msg = minion_msg::to_vec(&minion_msg::MinionMsg{ 
-        op: minion_msg::MinionOps::Auth,
-        payload: minion_msg::MinionId::new().into(),
+        op: MinionOps::Auth,
+        payload: MinionId::new().into(),
     }).unwrap();
     ws_stream.send(Message::Binary(msg)).await.expect("send failed");
 
@@ -34,7 +35,36 @@ async fn main() {
                     println!("server sent binary data");
                     let m = minion_msg::from_bytes(&b).unwrap();
                     println!("op: {:?}", m.op);
-                    
+                    match m.op { 
+                        MinionOps::Exec => {
+
+                            let store = Store::default();
+                            let module = Module::new(&store, &m.payload).unwrap();
+                            // The module doesn't import anything, so we create an empty import object.
+                            let import_object = imports! {};
+                            let instance = Instance::new(&module, &import_object).unwrap();
+
+                            let worker_func = instance.exports.get_function("run").unwrap();
+                            let result = worker_func.call(&[]).unwrap();
+
+                            ws_stream.send(
+                                Message::Binary(
+                                    minion_msg::to_vec(
+                                        &MinionMsg{
+                                        op: MinionOps::Ret,
+                                        payload: Vec::from_iter(
+                                            result.to_vec().
+                                            iter().map(
+                                                |x| x.i32().unwrap().to_ne_bytes()
+                                            ).flatten()
+                                        ), 
+                                    }).unwrap())
+                            ).await.expect("send failed");
+                        }
+                        _ => {
+                            println!("server bad {:?}", m.op);
+                        }
+                    }
                 }
                 Message::Ping(_) => {
                     println!("socket ping");
@@ -51,22 +81,9 @@ async fn main() {
                 }
             }
             Err(e) => {
-            println!("error {:?}", e);
-            return;
+                println!("error {:?}", e);
+                return;
             }
         }
-
-/*
-        let data = msg.unwrap().into_data();
-        let store = Store::default();
-        let module = Module::new(&store, &data).unwrap();
-        // The module doesn't import anything, so we create an empty import object.
-        let import_object = imports! {};
-        let instance = Instance::new(&module, &import_object).unwrap();
-
-        let worker_func = instance.exports.get_function("run").unwrap();
-        let result = worker_func.call(&[Value::I32(0)]).unwrap();
-        ws_stream.send(Message::text(result.to_vec()[0].to_string())).await.expect("send failed");
-*/
     }
 }
